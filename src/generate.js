cdb8b6c7472462563738cb3d80f4de48f89de8bb
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-09-29 12:11:46 -0400

    added manager section html box

--- src/generate.js
+++ src/generate.js
@@ -5,5 +5,21 @@ const teamGenerater = (team) => {
 
     const html = [];
 
-    const managerGenerator = Manager
+    const managerGenerator = manager => {
+        console.log(manager);
+        let managerHTML = `
+        <div class='card text-bg-primary m-4' style='width: 18rem;>
+        
+            <h5 class='card-title'>Name</h5>
+            <h6 class='card-subtitle mb-2'>Job Title: Manager</h6>
+            <div class='card-body>
+                ${manager.name} <br/>
+                <ul class='list-group list-group-flush'>
+                    <li class="list-group-item">ID:</li>
+                    <li class="list-group-item">Email</li>
+                    <li class="list-group-item">Office number:</li>
+                </ul>
+            </div>
+        </div>`
+    }
 }
